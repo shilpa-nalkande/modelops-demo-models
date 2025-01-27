0740102ff34c5ba969e2c8191e6b9a100d6f1dcf
shilpa-nalkande <shilpa.nalkande@teradata.com>
2025-01-27 06:00:45 +0000

    Added Feature Engineering task

--- feature_engineering_tasks/pima_data_prep/task.py
+++ feature_engineering_tasks/pima_data_prep/task.py
@@ -6,16 +6,20 @@ from sqlalchemy import literal_column
 
 
 def run_task(context: ModelContext, **kwargs):
-    aoa_create_context()
+    # aoa_create_context()
     df = DataFrame.from_query("SELECT * FROM DEMO_ModelOps.pima_patient_features")
     
 
     from sqlalchemy import literal_column
     from teradatasqlalchemy import DATE
     import numpy as np
+    from sqlalchemy import func
+    current_date_ = func.current_date()
     column = literal_column("CAST('1950-01-01' AS DATE) + (CAST('2000-01-01' AS DATE) - CAST('1950-01-01' AS DATE)) * RANDOM(1,100)", type_=DATE)
-    df = df.assign(birthday = column)
-    df = df.assign(calculated_age = (CURRENT_DATE - df.birthday)/365)
+    df = df.assign(current_dt_col = current_date_)
+    df = df.assign(birthday = (df.current_dt_col - df.Age * 365))
+    # return(df)
+    df = df.assign(calculated_age = (df.current_dt_col - df.birthday)/365)
     
 #     print(df)
 #     # Calculate age
@@ -23,10 +27,11 @@ def run_task(context: ModelContext, **kwargs):
     
 #     # Remove the original age column
 #     df_pd = df_pd.drop(columns=['Age'])
-    
+    # return(df)
     scale_fit = ScaleFit(
         data=df,
-        target_columns=["1:"],
+        target_columns=["NumTimesPrg","PlGlcConc","BloodP","SkinThick","TwoHourSerIns","BMI","DiPedFunc",
+                        "Age","calculated_age"],
         scale_method="RANGE",
         miss_value="KEEP",
         global_scale=False
@@ -47,10 +52,19 @@ def run_task(context: ModelContext, **kwargs):
     # Create a teradataml DataFrame from the table
     tdf = DataFrame('transformed_data')
     
-    print(df)
+#     Feature engineering
+    fs = FeatureStore(repo='demo_user')
+    fs.setup()
+    fg = FeatureGroup.from_DataFrame(name='PIMA', df=tdf, entity_columns='PatientId')
+    fs.apply(fg)
+    df = fs.get_dataset('PIMA')
+    # df
+    # print(df)
     with open(f"{context.artifact_output_path}/transformation_report.txt", "w") as f:
         print(tdf.describe(), file=f)
     
     # Store build properties as a file artifact
     with open(f"{context.artifact_output_path}/build_properties.txt", "w") as f:
         f.write(str(kwargs))
+        
+    # return(df)
