f10a9860a5757a6c6a7d17c3d60234f43591c8c0
shilpa-nalkande <shilpa.nalkande@teradata.com>
2025-10-07 06:26:02 +0000

    Added Telco Churn XGboost in database demo model

---
+++ model_definitions/telco_XGBoost/model_modules/scoring.py
@@ -0,0 +1,105 @@
+from teradataml import (
+    copy_to_sql,
+    DataFrame,
+    XGBoostPredict,
+    ScaleTransform,
+    ConvertTo,
+    translate
+)
+from aoa import (
+    record_scoring_stats,
+    aoa_create_context,
+    ModelContext
+)
+import pandas as pd
+from teradatasqlalchemy import INTEGER
+
+
+def score(context: ModelContext, **kwargs):
+    
+    aoa_create_context()
+
+    # Load the trained model from SQL
+    model = DataFrame(f"model_${context.model_version}")
+
+    # Extract feature names, target name, and entity key from the context
+    feature_names = context.dataset_info.feature_names
+    target_name = context.dataset_info.target_names[0]
+    entity_key = context.dataset_info.entity_key
+
+    # Load the test dataset
+    test_df = DataFrame.from_query(context.dataset_info.sql)
+    features_tdf = DataFrame.from_query(context.dataset_info.sql)
+    # features_pdf = features_tdf.to_pandas(all_rows=True)
+    
+    # Scaling the test set
+    print ("Loading scaler...")
+    scaler = DataFrame(f"scaler_${context.model_version}")
+
+    # Scale the test dataset using the trained scaler
+    scaled_test = ScaleTransform(
+        data=test_df,
+        object=scaler,
+        accumulate = entity_key
+    )
+    
+    print("Scoring...")
+    # Make predictions using the XGBoostPredict function
+    predictions = XGBoostPredict(
+        object=model,
+        newdata=scaled_test.result,
+        model_type = 'Classification',
+        id_column=entity_key,
+        output_prob=True,
+        output_responses=['0','1'],
+        object_order_column=['task_index', 'tree_num', 'iter', 'class_num', 'tree_order']
+    )
+    
+    
+    # store the predictions
+    predictions_df = predictions.result
+    predictions_pdf = predictions_df.assign(drop_columns=True,
+                                             job_id=translate(context.job_id),
+                                             CustomerID=predictions_df.CustomerID,
+                                             Churn=predictions_df.Prediction.cast(type_=INTEGER),
+                                             json_report=translate("  "))
+                                             
+    
+    
+    print("Finished Scoring")
+
+    
+#     # teradataml doesn't match column names on append.. and so to match / use same table schema as for byom predict
+#     # example (see README.md), we must add empty json_report column and change column order manually (v17.0.0.4)
+#     # CREATE MULTISET TABLE pima_patient_predictions
+#     # (
+#     #     job_id VARCHAR(255), -- comes from airflow on job execution
+#     #     PatientId BIGINT,    -- entity key as it is in the source data
+#     #     HasDiabetes BIGINT,   -- if model automatically extracts target
+#     #     json_report CLOB(1048544000) CHARACTER SET UNICODE  -- output of
+#     # )
+#     # PRIMARY INDEX ( job_id );
+#     predictions_pdf["json_report"] = ""
+#     predictions_pdf = predictions_pdf[["job_id", entity_key, target_name, "json_report"]]
+
+    copy_to_sql(
+        df=predictions_pdf,
+        schema_name=context.dataset_info.predictions_database,
+        table_name=context.dataset_info.predictions_table,
+        index=False,
+        if_exists="append"
+    )
+    
+    print("Saved predictions in Teradata")
+
+    # calculate stats
+    predictions_df = DataFrame.from_query(f"""
+        SELECT 
+            * 
+        FROM {context.dataset_info.get_predictions_metadata_fqtn()} 
+            WHERE job_id = '{context.job_id}'
+    """)
+
+    # record_scoring_stats(features_df=features_tdf, predicted_df=predictions_df, context=context)
+
+    print("All done!")
---
+++ model_definitions/telco_XGBoost/model_modules/training.py
@@ -0,0 +1,156 @@
+from teradataml import (
+    DataFrame,
+    XGBoost,
+    ScaleFit,
+    ScaleTransform,
+    Shap
+)
+
+from aoa import (
+    record_training_stats,
+    aoa_create_context,
+    ModelContext
+)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import json
+from collections import Counter
+
+
+def compute_feature_importance(feat_df):
+    df = feat_df.to_pandas()
+    df = df.T.reset_index()
+    df=df.rename(columns={'index': 'Feature', 0: 'Importance'})
+    df['Feature'] = df['Feature'].str.replace('TD_', '')
+    df['Feature'] = df['Feature'].str.replace('_SHAP', '')
+    return df
+
+
+def compute_feature_explain(explain_df):
+    explain_df = explain_df.drop(['CustomerID','Label','tree_num'],axis=1)
+    shap_mean = explain_df.agg(['min', 'max'])
+    df = shap_mean.to_pandas()
+    df = df.T.reset_index()
+    df=df.rename(columns={'index': 'Feature', 0: 'Importance'})
+    mean_positive = df[df['Importance'] > 0]
+    mean_negative = df[df['Importance'] < 0]
+    mean_positive['Feature'] = mean_positive.loc[:,'Feature'].str.replace('max_TD_', '')
+    mean_positive['Feature'] = mean_positive.loc[:,'Feature'].str.replace('_SHAP', '')
+    mean_negative['Feature'] = mean_negative.loc[:,'Feature'].str.replace('min_TD_', '')
+    mean_negative['Feature'] = mean_negative.loc[:,'Feature'].str.replace('_SHAP', '')
+    # mean_positive['Feature'] = mean_positive['Feature'].str.replace('max_TD_', '')
+    # mean_positive['Feature'] = mean_positive['Feature'].str.replace('_SHAP', '')
+    # mean_negative['Feature'] = mean_negative['Feature'].str.replace('min_TD_', '')
+    # mean_negative['Feature'] = mean_negative['Feature'].str.replace('_SHAP', '')
+    return mean_positive,mean_negative
+    
+
+def plot_feature_importance(df, img_filename):
+    df = df.sort_values(by="Importance", ascending=False)
+    # Plot the bar graph
+    plt.figure(figsize=(10, 8))
+    sns.barplot(x="Importance",y="Feature",data=df, palette="viridis")
+    plt.title("Feature Importance")
+    plt.xlabel("SHAP Importance Value")
+    plt.ylabel("Features")
+    plt.tight_layout()
+    fig = plt.gcf()
+    fig.savefig(img_filename, dpi=500)
+    plt.clf()
+    
+def plot_feature_explain(mean_positive,mean_negative, img_filename):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bar_width = 0.35
+
+    ax.barh(mean_positive["Feature"], mean_positive["Importance"],color='salmon', label='-1 (positive)') 
+    ax.barh(mean_negative["Feature"], mean_negative["Importance"],color='cyan', label='1 (negative)')
+    ax.set_xlabel("mean(|SHAP value|)")
+    ax.set_title("Mean shap for all samples")
+    ax.legend(title="sign")
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
+    # plt.show()
+    fig = plt.gcf()
+    fig.savefig(img_filename, dpi=500)
+    plt.clf()    
+    
+def train(context: ModelContext, **kwargs):
+    aoa_create_context()
+    
+    # Extracting feature names, target name, and entity key from the context
+    feature_names = context.dataset_info.feature_names
+    target_name = context.dataset_info.target_names[0]
+    entity_key = context.dataset_info.entity_key
+    categorical_features = ['intl_user_flag', 'contract_risk_score', 'payment_risk_score'] 
+    # Load the training data from Teradata
+    train_df = DataFrame.from_query(context.dataset_info.sql)
+    
+    print ("Scaling using InDB Functions...")
+    
+    # Scale the training data using the ScaleFit and ScaleTransform functions
+    scaler = ScaleFit(
+        data=train_df,
+        target_columns = feature_names,
+        scale_method = context.hyperparams["scale_method"],
+        miss_value = context.hyperparams["miss_value"],
+        global_scale = context.hyperparams["global_scale"].lower() in ['true', '1'],
+        multiplier = context.hyperparams["multiplier"],
+        intercept = context.hyperparams["intercept"]
+    )
+
+    scaled_train = ScaleTransform(
+        data=train_df,
+        object=scaler.output,
+        accumulate = [target_name,entity_key]
+    )
+    
+    scaler.output.to_sql(f"scaler_${context.model_version}", if_exists="replace")
+    print("Saved scaler")
+    
+    print("Starting training...")
+
+    # Train the model using XGBoost
+    model = XGBoost(
+        data = scaled_train.result,
+        input_columns = feature_names,
+        response_column = target_name,
+        model_type = context.hyperparams["model_type"],
+        lambda1 = context.hyperparams["lambda1"],
+    )
+
+    # Save the trained model to SQL
+    model.result.to_sql(f"model_${context.model_version}", if_exists="replace")  
+    print("Saved trained model")
+    
+    #Shap explainer 
+    Shap_out = Shap(data=scaled_train.result, 
+                object=model.result, 
+                id_column='CustomerID',
+                training_function="TD_XGBOOST", 
+                model_type="Classification",
+                input_columns=feature_names, 
+                detailed=True)
+    
+    feat_df = Shap_out.output_data
+    explain_df = Shap_out.result
+    # print(explain_df)
+
+ 
+    df = compute_feature_importance(feat_df)
+    plot_feature_importance(df, f"{context.artifact_output_path}/feature_importance")
+    pos_expl_df, neg_expl_df = compute_feature_explain(explain_df)
+    plot_feature_explain(pos_expl_df,neg_expl_df, f"{context.artifact_output_path}/feature_explainability")
+
+    # record_training_stats(
+    #     train_df,
+    #     features=feature_names,
+    #     targets=[target_name],
+    #     categorical=[target_name],
+    #     # feature_importance=feature_importance,
+    #     context=context
+    # )
+    
+    print("All done!")
